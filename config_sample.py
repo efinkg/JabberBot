06f26a40c232e06ef7272bcf03c9a7812f4f12fc
efinkg <efinkg@gmail.com>
2013-09-11 13:09:09 -0500

    Adding sample config file

---
+++ config_sample.py
@@ -0,0 +1,18 @@
+#Configuration File
+#This is where all your personal information goes
+#rename this file to config.py
+
+def emailSenderUsername(): #This is the username emails are sent from
+    return 'username@sample.com'
+
+def emailSenderPassword(): #This is the password to the email account emails are sent from
+    return 'password'
+
+def emailRecipient(): #This is where all the start and stop emails will go
+    return 'username@sample.com'
+
+def jabberUsername(): #This is the jabber account used for the JabberBot
+    return 'username@sample.com'
+
+def jabberPassword(): #This is the password for the jabber account used for the JabberBot
+    return 'password'

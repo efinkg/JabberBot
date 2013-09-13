bcbcc9aa5815edf227daff0d25213ae46a343521
efinkg <efinkg@gmail.com>
2013-09-13 02:16:02 -0500

    Fix total coffee counter with pickle

--- howMuch.py
+++ howMuch.py
@@ -1,8 +1,10 @@
 import sys
+import pickle
 
-coffeeLiter = 0.
+coffeeLiter = 0. #Clears variables.  Honestly, probably not important
 coffeeOunces = 0.
-try:
+
+try:    #makes sure that coffeeLiterTotal and coffeeOuncesTotal are initialized
     coffeeLiterTotal
 except NameError:
     coffeeLiterTotal= 0.
@@ -11,25 +13,37 @@ try:
 except NameError:
     coffeeOuncesTotal = 0.
 
-def makingCoffee(ounce):
-    global coffeeOunces
+def makingCoffee(ounce): #Pass variable 'ounce' in from JabberBot
+    global coffeeOunces #Global variables
     global coffeeLiter
     global coffeeOuncesTotal
     global coffeeLiterTotal
-    coffeeOunces = coffeeOunces + ounce
-    coffeeOuncesTotal = coffeeOuncesTotal + ounce
-    coffeeLiter = coffeeOunces/33.8
-    coffeeLiterTotal = coffeeOuncesTotal/33.8
+
+    #Loads current value of coffeeLiterTotal
+    pkl_file = open('totalCoffee.pkl', 'rb') #Open pickle file 'totalCoffee'
+    coffeeLiterTotal = pickle.load(pkl_file) #write current value of 'totalCoffee' to coffeeLiterTotal
+    pkl_file.close() #Close pickle file 'totalCoffee'
+    
+    coffeeOunces = coffeeOunces + ounce #Increment coffeeOunces
+    coffeeOuncesTotal = coffeeOuncesTotal + ounce #Increment coffeeOuncesTotal
+    coffeeLiter = coffeeLiter+ coffeeOunces/33.8 #Calculate liters from ounces and add to current coffeeLiter
+    coffeeLiterTotal = coffeeLiterTotal + coffeeOuncesTotal/33.8 #Calculate liters from ounces and add to current coffeeLiterTotal
+
+    #Writes current value of coffeeLiterTotal
+    output = open('totalCoffee.pkl', 'wb') #Open pickle file 'totalCoffee'
+    pickle.dump(coffeeLiterTotal, output) #write current value of coffeeLiterTotal 'totalCoffee'
+    output.close() #Close pickle file 'totalCoffee'
 
 def coffeeMade():
-    global cofeeLiter
+    global cofeeLiter #Global variable
     return coffeeLiter
 
 def coffeeMadeTotal():
-    global coffeeLiterTotal
+    global coffeeLiterTotal #Global variable
+    
+    #Loads current value of coffeeLiterTotal
+    pkl_file = open('totalCoffee.pkl', 'rb') #Open pickle file 'totalCoffee'
+    coffeeLiterTotal = pickle.load(pkl_file) #write current value of 'totalCoffee' to coffeeLiterTotal
+    pkl_file.close() #Close pickle file 'totalCoffee'
+    
     return coffeeLiterTotal
-
-def initialize():
-    global coffeeOunces
-    print 'hi'
-    coffeeOunces = 0

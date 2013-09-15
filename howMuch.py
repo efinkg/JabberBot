ca4ac7adbb49fc65f853d86251e154e8633100a6
efinkg <efinkg@gmail.com>
2013-09-14 21:28:39 -0500

    Fix howMuch counter

--- howMuch.py
+++ howMuch.py
@@ -2,7 +2,6 @@ import sys
 import pickle
 
 coffeeLiter = 0. #Clears variables.  Honestly, probably not important
-coffeeOunces = 0.
 
 try:    #makes sure that coffeeLiterTotal and coffeeOuncesTotal are initialized
     coffeeLiterTotal
@@ -14,21 +13,19 @@ except NameError:
     coffeeOuncesTotal = 0.
 
 def makingCoffee(ounce): #Pass variable 'ounce' in from JabberBot
-    global coffeeOunces #Global variables
-    global coffeeLiter
-    global coffeeOuncesTotal
+    global coffeeLiter #Global variables
     global coffeeLiterTotal
-
+    
+    print str(ounce)
     #Loads current value of coffeeLiterTotal
     pkl_file = open('totalCoffee.pkl', 'rb') #Open pickle file 'totalCoffee'
     coffeeLiterTotal = pickle.load(pkl_file) #write current value of 'totalCoffee' to coffeeLiterTotal
     pkl_file.close() #Close pickle file 'totalCoffee'
-    
-    coffeeOunces = coffeeOunces + ounce #Increment coffeeOunces
-    coffeeOuncesTotal = coffeeOuncesTotal + ounce #Increment coffeeOuncesTotal
-    coffeeLiter = coffeeLiter+ coffeeOunces/33.8 #Calculate liters from ounces and add to current coffeeLiter
-    coffeeLiterTotal = coffeeLiterTotal + coffeeOuncesTotal/33.8 #Calculate liters from ounces and add to current coffeeLiterTotal
 
+    coffeeLiter = coffeeLiter + ounce/33.8 #Calculate liters from ounces and add to current coffeeLiter
+    coffeeLiterTotal = coffeeLiterTotal + ounce/33.8 #Calculate liters from ounces and add to current coffeeLiterTotal
+    print str(coffeeLiter)
+    print str(coffeeLiterTotal)
     #Writes current value of coffeeLiterTotal
     output = open('totalCoffee.pkl', 'wb') #Open pickle file 'totalCoffee'
     pickle.dump(coffeeLiterTotal, output) #write current value of coffeeLiterTotal 'totalCoffee'
@@ -39,7 +36,7 @@ def coffeeMade():
     return coffeeLiter
 
 def coffeeMadeTotal():
-    global coffeeLiterTotal #Global variable
+    #global coffeeLiterTotal #Global variable
     
     #Loads current value of coffeeLiterTotal
     pkl_file = open('totalCoffee.pkl', 'rb') #Open pickle file 'totalCoffee'
